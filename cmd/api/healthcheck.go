0fa0083ca6c6390348693e9a916142fb13fb9916
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-18 22:29:50 +0400

    Add - Envelope the movie struct with the title

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -10,7 +10,8 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	err := app.writeJSON(w, http.StatusOK, envelope{"movie": data}, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
