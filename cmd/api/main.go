3e6f8bdf1c9ffc06149ebccf4b8125d8e5433ff9
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-22 21:21:11 +0400

    Fix - Updated import for the data in  file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"greenlight.giorgiubiria.ge/internal/data"
 )
 
 const version = "1.0.0"
@@ -60,7 +61,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
-    models: data.NewModels(db)
+    models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
