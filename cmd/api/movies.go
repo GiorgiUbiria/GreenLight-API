508a297b859ac71a88e6e21728f88bc788a2906e
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-24 19:34:42 +0400

    Fix - Modified the input field to include data.Fields instead of directly assigning field values

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -13,9 +13,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	var input struct {
 		Title    string
 		Genres   []string
-		Page     int
-		PageSize int
-		Sort     string
+    data.Filters
 	}
 
 	v := validator.New()
@@ -26,11 +24,11 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 
 	input.Genres = app.readCSV(qs, "genres", []string{})
 
-	input.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
 
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
-	input.Sort = app.readString(qs, "sort", "id")
+	input.Filters.Sort = app.readString(qs, "sort", "id")
 
 	if !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
